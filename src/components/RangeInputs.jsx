075ac4421b18a98af3fd0834acf1452479712272
abhishekD97 <abhishekdolli@gmail.com>
2021-02-12 01:33:09 +0530

    added range elements with console log

--- src/components/RangeInputs.jsx
+++ src/components/RangeInputs.jsx
@@ -2,20 +2,58 @@ import React, {useState} from "react";
 import RangeSlider from 'react-bootstrap-range-slider';
 
 function RangeInputs(){
-    const [ sliderValue, setSliderValue ] = useState(0);
-
-    function sliderFunction(e){
-        setSliderValue(e.target.value);
+    const [ sliderValueOne, setSliderValueOne ] = useState(0);
+    const [ sliderValueTwo, setSliderValueTwo ] = useState(0);
+    const [ sliderValueThree, setSliderValueThree ] = useState(0);
+// Slider One
+    function sliderFunctionOne(e){
+        setSliderValueOne(e.target.value);
     }
-    function sliderFunctionSet(e){
-        setSliderValue(e.target.value);
+    function sliderFunctionSetOne(e){
+        setSliderValueOne(e.target.value);
+        console.log(e.target.value)
+    }
+// Slider Two
+    function sliderFunctionTwo(e){
+        setSliderValueTwo(e.target.value);
+    }
+    function sliderFunctionSetTwo(e){
+        setSliderValueTwo(e.target.value);
+        console.log(e.target.value)
+    }
+// Slider Three
+    function sliderFunctionThree(e){
+        setSliderValueThree(e.target.value);
+    }
+    function sliderFunctionSetThree(e){
+        setSliderValueThree(e.target.value);
         console.log(e.target.value)
     }
 
+
+
     return (
-        <div className="col-md-4">
+        <div>
             <p className="formHeader col-md-2">Range Inputs</p>
-            <RangeSlider value={sliderValue} onChange={sliderFunction} onAfterChange={sliderFunctionSet}/>
+            <br/>
+            <div className="row centerJustify">
+            <div className="col-md-5">
+            <RangeSlider value={sliderValueOne} onChange={sliderFunctionOne} onAfterChange={sliderFunctionSetOne}/>
+            </div>
+            <div className="col-md-1"></div><br/>
+            <div className="col-md-5">
+            <RangeSlider value={sliderValueTwo} onChange={sliderFunctionTwo} onAfterChange={sliderFunctionSetTwo}/>
+            </div>
+            </div>
+            <br/>
+            <div className="row centerJustify">
+            <div className="col-md-5">
+            <RangeSlider value={sliderValueThree} onChange={sliderFunctionThree} onAfterChange={sliderFunctionSetThree}/>
+            </div>
+            <div className="col-md-1"></div>
+            <div className="col-md-5">
+            </div>
+            </div>
         </div>
     );
 }
